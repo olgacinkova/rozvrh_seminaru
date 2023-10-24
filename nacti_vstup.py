4550b0579e4f1b3475d1299c10ceed917b3f93ba
Olga Cinkova <cinkovaolga@gmail.com>
2023-10-24 18:56:46 +0200

    nacti_zaky function add

---
+++ nacti_vstup.py
@@ -0,0 +1,21 @@
+def nacti_zaky(soubor):
+    # nacita vstupni soubor zaci.csv
+    with open(soubor, encoding='utf-8-sig') as infile:
+        lines = infile.readlines()
+        zaci = []
+    for x in lines:
+        radek = x.rstrip("\n")
+        radek = radek.replace("'","")
+        radek = radek.split(";")
+        id, jmeno, trida, uzivjmeno = radek
+        zaci.append(id, trida)
+    print(zaci)
+    return
+
+
+def main():
+    nacti_zaky("zaci")
+    return
+
+if __name__ == "__main__":
+    main()
