bfef2c5fc51fdb6ce342f04fa98abeca15ed0bb2
Olga Cinkova <cinkovaolga@gmail.com>
2023-10-24 20:12:50 +0200

    nacti zapsane function

--- nacti_vstup.py
+++ nacti_vstup.py
@@ -12,6 +12,20 @@ def nacti_zaky(soubor):
     print(zaci)
     return
 
+def nacti_zapsane(soubor):
+    # nacita vstupni soubor zapsani.csv
+    with open(soubor, encoding='utf-8-sig') as infile:
+        lines = infile.readlines()
+        zapsani = []
+    for x in lines:
+        radek = x.rstrip("\n")
+        radek = radek.replace("'","")
+        radek = radek.split(";")
+        id, jmeno, trida, uzivjmeno = radek
+        zaci.append(id, trida)
+    print(zaci)
+    return
+
 
 def main():
     nacti_zaky("zaci")
