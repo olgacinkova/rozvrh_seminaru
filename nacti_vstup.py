35509e796322f249fa148e6ff4e4eeef50f7cb2d
Olga Cinkova <cinkovaolga@gmail.com>
2023-10-29 13:50:58 +0100

    nacti_zapsane function

--- nacti_vstup.py
+++ nacti_vstup.py
@@ -13,7 +13,7 @@ def nacti_zaky(soubor):
     return
 
 def nacti_zapsane(soubor):
-    # nacita vstupni soubor zapsani.csv
+    # nacita vstupni soubor zapsani
     # vystupem je pole poli, kde je vzdy zak a jeho seminar
     with open(soubor, encoding='utf-8-sig') as infile:
         lines = infile.readlines()
@@ -22,16 +22,38 @@ def nacti_zapsane(soubor):
         radek = x.rstrip("\n")
         radek = radek.replace("'","")
         radek = radek.split(";")
-        print(radek)
         zak = int(radek[1])
         seminar = int(radek[2])
         zapsani.append([zak, seminar])
-    print(zapsani)
-    return
+    #print(zapsani)
+    return zapsani
+
+def zak_trida(soubor):
+    # nacita soubor zaci
+    #do jakych trid chodi zaci
+    # vystup: dict, trida : mnozina jejich zaku
+    with open(soubor, encoding='utf-8-sig') as infile:
+        lines = infile.readlines()
+        tridy = dict()
+    for x in lines:
+        radek = x.rstrip("\n")
+        radek = radek.replace("'","")
+        radek = radek.split(";")
+        trida = radek[2].replace(" ", "") # odstranim mezery z nazvu tridy
+        zak = int(radek[0]) # resp id zaka
+        if trida in tridy:
+            tridy[trida].add(zak) # prida do mnoziny noveho zaka
+        else:
+            tridy[trida] =set() # vytvori novou prazdnou mnozinu
+            tridy[trida].add(zak) # prida do mnoziny rovnou prvniho zaka
+    return tridy
+        
+
 
 
 def main():
     nacti_zapsane("zapsani")
+    print(zak_trida("zaci"))
     return
 
 if __name__ == "__main__":
