3619e135346c669831570b87f38da2baa0a4c59c
Olga Cinkova <cinkovaolga@gmail.com>
2023-11-08 11:45:15 +0100

    komentare

--- nacti_vstup.py
+++ nacti_vstup.py
@@ -4,8 +4,9 @@ import matplotlib.colors as mpl
 import networkx as nx
 from itertools import combinations
 
-def nacti_zaky(soubor):
+"""def nacti_zaky(soubor):
     # nacita vstupni soubor zaci.csv
+    # výstupem 
     df = pd.read_csv(soubor)
     id = list(df.id) # id zaku
     tridy = list(df.trida) # kam patri
@@ -14,91 +15,93 @@ def nacti_zaky(soubor):
         zak = id.pop(0)
         trida = tridy.pop(0)
         zaci[zak] = trida
+        print(zaci)
     return zaci
-
+"""
 
 def zaci_tridy(soubor):
-    # nacita soubor zaci
-    #do jakych trid chodi zaci
-    # vystup: dict, trida : mnozina jejich zaku
+    # načítá soubor zaci.csv
+    # do jakých tříd chodí žáci
+    # výstup: dictionary ve formátu třída : množina jejích žáků
     df = pd.read_csv(soubor)
-    id = list(df.id) # id zaku
-    tridy = list(df.trida) # kam patri
-    kam_trida = dict()
-    for i in range(len(id)):
+    id = list(df.id) # id žáků
+    tridy = list(df.trida) # do jaké třídy patří
+    kam_trida = dict() # dictionary, kdo kam chodí
+    for i in range(len(id)): # projdu všechny žáky
         trida = tridy.pop(0).replace(" ", "")
-        zak = id. pop(0) #id zaka
-        if trida in kam_trida:
-            kam_trida[trida].add(zak) # prida do mnoziny noveho zaka
+        zak = id. pop(0) #id prvního žáka ze seznamu
+        if trida in kam_trida: # pokud už je třída v dictionary
+            kam_trida[trida].add(zak) # přidá do množiny nového žáka
         else:
-            kam_trida[trida] =set() # vytvori novou prazdnou mnozinu
-            kam_trida[trida].add(zak) # prida do mnoziny rovnou prvniho zaka
+            kam_trida[trida] =set() # vytvoří novou prázdnou množinu pro třídu
+            kam_trida[trida].add(zak) # přidá do množiny rovnou prvního žáka
     return kam_trida
 
 def zaci_seminare(soubor):
-    # nacita vstupni soubor zapsani.csv
-    # vystupem je dict, kde je vzdy zak a jeho seminare
-    df = pd.read_csv(soubor)
-    id = list(df.zak) # id zaka
-    seminare = list(df.seminar) # kam patri
-    kam_seminar = dict()
-    for i in range(len(id)):
-        zak = id.pop(0)
-        seminar = seminare.pop(0)
-        if zak in kam_seminar:
-            kam_seminar[zak].add(seminar) # prida do mnoziny novy seminar
+    # načítá vstupní soubor zapsani.csv
+    # výstupem je dict, kde je vždy zak množina seminářů, kam chodí
+    df = pd.read_csv(soubor) # načtu soubor jako dataframe
+    id = list(df.zak) # seznam id žáků (sloupec)
+    seminare = list(df.seminar) # seznam seminářů žáků (sloupec)
+    kam_seminar = dict() # výstupní dictionary
+    for i in range(len(id)): # pro každého žáka
+        zak = id.pop(0) # aktuální žák
+        seminar = seminare.pop(0) # seminář, na který chodí
+        if zak in kam_seminar: # pokud už žák má množinu v dictu
+            kam_seminar[zak].add(seminar) # přidám do množiny nový seminář
         else:
-            kam_seminar[zak] =set() # vytvori novou prazdnou mnozinu
-            kam_seminar[zak].add(seminar) 
+            kam_seminar[zak] =set() # vytvoří novou prázdnou množinu
+            kam_seminar[zak].add(seminar) # přidám tam nový seminář
     return kam_seminar
 
 def id_ucitelu(soubor):
-    # bere na vstupu seznam seminaru s uciteli
-    # ke kazdemu uciteli vymysli id cislo
-    # vystup dict, kde je ke kazdemu uciteli 
-    # mnozina seminaru, ktere uci
-    df = pd.read_csv(soubor)
-    s = list(df.id) # id seminaru
-    id_seminaru = list(df.id)
-    j= list(df.ucitel) # jmena ucitelu
-    jmena = set()
+    # bere na vstupu seznam seminářů s učiteli
+    # ke každému učiteli vymyslí id čísl
+    # o
+    # výstup dict, kde je ke každému učiteli množina seminářů, které učí
+    df = pd.read_csv(soubor) # načtu seznam seminářů jako dataframe
+    id_seminaru = list(df.id) # id seminářů
+    j= list(df.ucitel) # jména učitelů
+    jmena = set() # množina jmen učitelů
     for jm in j:
         for x in jm.split(","): # obcas je nekde vic ucitelu u jednoho seminare
             x = x.replace(" ","")
             jmena.add(x)
-    ucitele = dict()
+    ucitele = dict() # dict, kde je učitel a k němu množina jeho seminářů
     i = 1
-    for jm in jmena:
+    for jm in jmena: # každému učiteli vymyslí jeho id
         ucitele[jm] = i
         i += 1
-    ## odstranim to, co tam dela neplechu, co je nejednoznacne
-    seminare = dict()
-    for x in range(len(s)):
-        seminar = s.pop(0)
-        ucitel = j.pop(0)
-        ucitel = ucitel.replace(' ','') 
-        ucitel = ucitel.split(",") # blbne to, kdyz je ucitelu u jednoho seminare vic nez jeden
-        print(ucitel)
-        if type(ucitel) == list:
-            for x in ucitel:
-                id_ucitele = ucitele[x]
-                if id_ucitele in seminare:
-                    seminare[id_ucitele].add(seminar)
-                else:
-                    seminare[id_ucitele] = set()
-                    seminare[id_ucitele].add(seminar)
+
+    seminare = dict() # dictionary, kde je vždy učitel k němu množina jeho seminářů
+    for x in range(len(id_seminaru)): # pro každý seminář
+        seminar = id_seminaru.pop(0) # aktuální seminář
+        ucitel = j.pop(0) # aktuální učitel
+        ucitel = ucitel.replace(' ','' ) # odstraním pro jistotu mezery ze jmen učitelů 
+        ucitel = ucitel.split(",") # u některých seminářů je víc učitelů - rozdělím je
+        if type(ucitel) == list: # pokud víc než jeden učitel
+            for x in ucitel: # vezmu jednoho učitele
+                id_ucitele = ucitele[x] # kouknu na jeho id
+                if id_ucitele in seminare: # pokud má už množinu svých seminářů
+                    seminare[id_ucitele].add(seminar) # přidám další seminář
+                else: # pokud učitel ještě nemá množinu svých seminářů
+                    seminare[id_ucitele] = set() # vytvořím mu prázdnou množinu
+                    seminare[id_ucitele].add(seminar) # rovnou do množiny přidám seminář
         else:
-            id_ucitele = ucitele[ucitel]
-            if id_ucitele in seminare:
+            id_ucitele = ucitele[ucitel] # vezmu aktuálního učitele
+            if id_ucitele in seminare: # to stejné, co v ifu výše
                 seminare[id_ucitele].add(seminar)
             else:
                 seminare[id_ucitele] = set()
                 seminare[id_ucitele].add(seminar)
-
+    # vrací dict učitelů a jejich id, dict učitelů a množin jejich seminářů, seznam id seminaru
     return ucitele, seminare, id_seminaru
 
 def udelej_graf(ucitele, seminare, id_seminaru, kam_seminar):
-    P = nx.MultiGraph() # ma neorientovane grafy
+    # tvorba neorientovaného grafu, kde vrcholy jsou semináře
+    # semináře budou spojeny hranou, pokud sdílí žáka nebo učitele
+    # hrany jsou ohodnocené: žák má hodnotu 1, učitel má hodnotu 100
+    P = nx.MultiGraph() # multigraf = má mezi dvojicí vrcholů víc než dvě hrany
     P.add_nodes_from(id_seminaru)
 
     ### profesorske hrany
