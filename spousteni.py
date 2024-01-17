6f45a701974e4f46e3b3fff0dd359a31bedb0bf6
Olga Cinkova <cinkovaolga@gmail.com>
2024-01-17 19:46:39 +0100

    seminare-fix-format je ted seminare.csv

--- spousteni.py
+++ spousteni.py
@@ -1,6 +1,15 @@
 from nacti_vstup import *
 from barveni import *
 
+class Seminar:
+    def __init__(self, id_seminare):
+        self.__id_seminare = id_seminare 
+        self.pro_ktere_rocniky: int = None
+        self.kteri_zaci_tam_chodi: set = set()
+        self.kdo_seminar_uci: list = []
+    def uloz_pro_ktere_rocniky(self, soubor): # nacita soubor seminare.csv
+        
+
 class Rocnik:
     def __init__(self, kolikaty): # kvuli spojeni kvinty a sexty
         if type(kolikaty) == int:
@@ -11,17 +20,18 @@ class Rocnik:
         else:
             raise Exception("Spatny argument")
         
+        id_ucitelu_rocniku: dict = dict()
         self.zaci: set = set() # mnozina zaku, kteri chodi do daneho rocniku
-        self.seminare: set = set() # kdo chodi na ktery seminar
+        self.zaci_kam_na_seminare: dict = dict() # dictionary ktery zak chodi na ktery seminar
         self.ucitele: set = set()  # mnozina ucitelu uci seminare daneho rocniku
         self.id_seminaru_rocniku: list = []
-    def uloz_zaky(self, zaci_rocniku):
-        for rocnik in self.__kolikaty:
-            self.zaci.union(zaci_rocniku[rocnik])
     
-    def uloz_seminare(self, zaci_seminaru):
+    
+    """def uloz_zaci(self, zaci_rocniku):
         for rocnik in self.__kolikaty:
-            self.seminare.union(zaci_seminaru[rocnik])
+            self.zaci.union(zaci_rocniku[rocnik])"""
+    def uloz_zaci(self):
+        self.zaci = self.zaci_kam_na_seminare.keys()
     
     def uloz_ucitele(self, ucitele_seminaru):
         for rocnik in self.__kolikaty:
@@ -38,8 +48,8 @@ class Rocnik:
     
     def uloz_data_pro_rocnik(self, zaci_rocniku, zaci_seminaru,
                              ucitele_seminaru, seminare_rocniky):
-        self.uloz_zaky(zaci_rocniku)
-        self.uloz_seminare(zaci_seminaru)
+        self.uloz_zaci_kam_na_seminare(zaci_seminaru)
+        self.uloz_zaci(zaci_rocniku)
         self.uloz_ucitele(ucitele_seminaru)
         self.uloz_id_seminaru_rocniku(seminare_rocniky)
 
@@ -49,11 +59,11 @@ def main():
 
     zaci_seminaru = nacti_zaky_seminaru("zapsani")
     zaci_rocniku = nacti_zaky_rocniku("zaci")
-    id_vsech_seminaru = nacti_id_vsech_seminaru("seminare-fix-format.csv")
-    id_vsech_ucitelu = nacti_id_vsech_ucitelu("seminare-fix-format.csv")
-    seminare_rocniku = ktery_seminar_pro_ktery_rocnik("seminare-fix-format.csv")
-    ucitele_seminaru = nacti_ucitele_seminaru("seminare-fix-format.csv")
-    seminare_rocniky = ktery_seminar_pro_ktery_rocnik("seminare-fix-format.csv")
+    id_vsech_seminaru = nacti_id_vsech_seminaru("seminare.csv")
+    id_vsech_ucitelu = nacti_id_vsech_ucitelu("seminare.csv")
+    seminare_rocniku = ktery_seminar_pro_ktery_rocnik("seminare.csv")
+    ucitele_seminaru = nacti_ucitele_seminaru("seminare.csv")
+    seminare_rocniky = ktery_seminar_pro_ktery_rocnik("seminare.csv")
    
     # instance: jednotlive rocniky
     kvinta_sexta = Rocnik([5,6])
@@ -62,7 +72,12 @@ def main():
                              ucitele_seminaru, seminare_rocniky)
     breakpoint()
     septima = Rocnik(7)
+    septima.uloz_data_pro_rocnik(zaci_rocniku, zaci_seminaru,
+                             ucitele_seminaru, seminare_rocniky)
+    breakpoint()
     oktava = Rocnik([8])
+    oktava.uloz_data_pro_rocnik(zaci_rocniku, zaci_seminaru,
+                             ucitele_seminaru, seminare_rocniky)
 
 
     # kvinta a sexta muzou byt jako jedna instance, protoze s nimi manipuluji vzdy zaroven
